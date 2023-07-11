0a01d5d90e3555e62bbea3c4189723beaac22f6a
Ispas Florian <ispas.florian@gmail.com>
2023-07-11 19:20:55 +0300

    tema curs12 selenium v.2

--- src/main/java/tests/PostCommentInGalleryFormatTest.java
+++ src/main/java/tests/PostCommentInGalleryFormatTest.java
@@ -22,22 +22,17 @@ public class PostCommentInGalleryFormatTest extends BaseTests{
 		Thread.sleep(1000);
 		js.executeScript("document.getElementsByClassName('post_title')[4].firstElementChild.click()");
 
-		js.executeScript("document.getElementById('send_comment').scrollIntoView()");
-//		js.executeScript("document.getElementsByClassName('section_title comments_form_title').scrollIntoView()");
+		js.executeScript("document.getElementsByClassName('section_title comments_form_title')[0].scrollIntoView()");
 		Thread.sleep(1000);
-		js.executeScript("document.getElementById('comment').textContent='test message 17'");	
+		js.executeScript("document.getElementById('comment').textContent='test message 19'");	
 		js.executeScript("document.getElementById('author').value='florin'");	
 		js.executeScript("document.getElementById('email').value='florin_test@gmail.com'");
 		Thread.sleep(1000);
 		js.executeScript("document.getElementById('send_comment').click()");
 		
 		Thread.sleep(2000);		
-//		String comment = js.executeScript("return document.getElementsByClassName('comment_text').firstElementChild.innerHTML").toString();
-//		System.out.println(comment);
-		
-//		String confirmationText = js.executeScript("return document.getElementsByClassName('comment_not_approved').innerHTML").toString();
-//		System.out.println(confirmationText);
-		
+		String comment = js.executeScript("return document.getElementsByClassName('comment_text')[0].childNodes[0].innerHTML").toString();
+		System.out.println(comment);		
 	}
 
 }
